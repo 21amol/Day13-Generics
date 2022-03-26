5590d8f279220e6da5d69135e22d07ce7b902d44
Amol Ghotale <ghotaleamol@gmail.com>
2022-03-26 22:09:52 +0530

    ADDED: Max float out of three.

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class Generics {
 
-    public static Integer compareTo(Integer a, Integer b, Integer c) {
+    public static Integer compareToInteger(Integer a, Integer b, Integer c) {
         if (a > b && a > c) {
             return a;
         } else if (b > c) {
@@ -12,9 +12,21 @@ public class Generics {
         }
     }
 
+    public static Float compareToFloat(Float a, Float b, Float c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+
     public static void main(String[] args) {
         // Welcome to Generics program.
 
-        System.out.println("Maximun number out of three is: " + compareTo(5, 7, 8));
+        System.out.println("Maximum number out of three is: " + compareToInteger(5, 7, 8));
+        System.out.println("Maximum number out of three is: " + compareToFloat(-5f, -7f, -8f));
     }
 }
